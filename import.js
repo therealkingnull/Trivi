97e2f8a875e212fc502ad94b41e9e37dfee0609c
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2020-11-02 18:10:26 -0500

    Testing

--- import.js
+++ import.js
@@ -1,7 +1,16 @@
 const data = require('./data/trivia.json');
-
+const _ = require('lodash');
 const randomTriviaQuestion = () => {
   return data[Math.floor(Math.random() * data.length)];
+}
+
+const searchByDifficulty = () => {
+    let args = window.location.href.substring(52);
+    var result = _.find(data.difficulty, {'difficulty': args});
+        if (!result)
+            console.log('Nothing found');
+        else
+            console.log("got something");
 }
 
 module.exports = {data, randomTriviaQuestion}
