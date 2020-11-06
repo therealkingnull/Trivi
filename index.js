ce07faffc7aac53d65ad6e38b5db81afdca537fc
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2020-11-05 19:32:01 -0500

    Updates

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/difficulty=:difficulty', (req, res) => {
   });
 
 app.get('/category=:category', (req, res) => {
-    res.json(searchByCategory(req.perams.category, 1))
+    res.json(searchByCategory(req.params.category, 1))
 });
 
 app.get('/difficulty=:difficulty&category=:category', (req, res) => {
